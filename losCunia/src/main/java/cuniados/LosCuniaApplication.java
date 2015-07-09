52c4039b16a1e7030a689599c5847148d8d59a64
Facu Manzi <user@192.168.1.13>
2015-07-09 11:18:32 -0300

    prueba

---
+++ losCunia/src/main/java/cuniados/LosCuniaApplication.java
@@ -0,0 +1,12 @@
+package cuniados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosCuniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LosCuniaApplication.class, args);
+    }
+}
---
+++ losCunia/src/test/java/cuniados/LosCuniaApplicationTests.java
@@ -0,0 +1,18 @@
+package cuniados;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LosCuniaApplication.class)
+@WebAppConfiguration
+public class LosCuniaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
